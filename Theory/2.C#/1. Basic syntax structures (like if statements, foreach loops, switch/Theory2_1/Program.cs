114da9183d43741cd923c3699a06e65d2a689c10
Roman <Newtrr@yandex.ru>
2024-10-15 16:29:38 +0300

    via upload

---
+++ Theory/2.C#/1. Basic syntax structures (like if statements, foreach loops, switch/Theory2_1/Program.cs
@@ -0,0 +1,37 @@
+﻿string? userInput;
+int number = 0;
+
+Console.WriteLine("Please input a number:");
+userInput = Console.ReadLine();
+
+try
+{
+  number = int.Parse(userInput);
+}
+catch
+{
+    Console.WriteLine("Your input value is not number!");
+    Environment.Exit(1);
+}
+finally
+{
+    if(userInput.Contains('-'))
+    {
+        Console.WriteLine("Your number is negative");
+    }
+    if(userInput == "0")
+    {
+        Console.WriteLine("Your number is zero");
+    }
+    if(!userInput.Contains('-') && userInput != "0")
+    {
+        Console.WriteLine("Your number is positive");
+    }
+
+    Console.WriteLine($"Let's start to count from 1 to {number}:");
+
+    foreach (var item in Enumerable.Range(1, number))
+    {
+        Console.WriteLine(item);
+    }
+}
