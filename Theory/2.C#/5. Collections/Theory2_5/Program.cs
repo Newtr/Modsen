7352228eb74e233f51a34e330bbff51ef4129a58
Roman <Newtrr@yandex.ru>
2024-10-15 17:51:45 +0300

    via upload

---
+++ Theory/2.C#/5. Collections/Theory2_5/Program.cs
@@ -0,0 +1,64 @@
+﻿List<Student> myStudents = new List<Student>();
+myStudents.Add(new Student("Ivan",16));
+myStudents.Add(new Student("Kate",18));
+myStudents.Add(new Student("Petr",20));
+myStudents.Add(new Student("Andrey",22));
+
+foreach (var item in myStudents)
+{
+    Console.WriteLine(item.Name);
+}
+
+int arrLength = myStudents.ToArray().Length;
+
+for (int i = 0; i < arrLength; i++)
+{
+    if (myStudents[i].Name == "Petr")
+    {
+        myStudents.RemoveAt(i);
+        break;
+    }
+}
+
+foreach (var item in myStudents)
+{
+    Console.WriteLine(item.Name);
+}
+
+public class Student
+{
+    public string Name {get;set;}
+    public int Age {get; set;}
+
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+}
+
+// public class Listik<T>
+// {
+//     List<Student> myList = new List<Student>();
+
+//     public void AddStudent(Student obj)
+//     {
+//         myList.Add(obj);
+//     }
+
+//     public void DeleteStudent(Student obj)
+//     {
+//         myList.Remove(obj);
+//     }
+
+//     public void ShowAllStudents()
+//     {
+//         Console.WriteLine("All students:");
+//         foreach (Student item in myList)
+//         {
+//             Console.WriteLine(item)
+//         }
+//     }
+    
+// }
