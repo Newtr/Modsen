dab95a121eb7ccc4d37939832d28ade60a0dd102
Roman <Newtrr@yandex.ru>
2024-10-15 17:11:29 +0300

    via upload

---
+++ Theory/2.C#/3. Exception handling/Theory2_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Transactions;
+
+int number1;
+int number2;
+int result;
+
+try
+{
+    Console.WriteLine("Enter first number: ");
+    number1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter second number: ");
+    number2 = int.Parse(Console.ReadLine());
+    result = number1/number2;
+}
+catch(DivideByZeroException)
+{
+    Console.WriteLine("You can't divide by zero");
+    Environment.Exit(221);
+}
+catch(FormatException)
+{
+    Console.WriteLine("Input value must be integer!");
+    Environment.Exit(223);
+}
+finally
+{
+    Console.WriteLine($"All good!");
+}
