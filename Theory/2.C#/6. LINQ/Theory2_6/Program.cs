6254b6a0cbc6885294e6934891ffa793ed5f4b6b
Roman <Newtrr@yandex.ru>
2024-10-16 18:19:53 +0300

    via upload

---
+++ Theory/2.C#/6. LINQ/Theory2_6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+List<Employee> myEmployees = new List<Employee>
+{
+    new Employee("Maks", 20),
+    new Employee("Kirill", 35),
+    new Employee("Fedor", 45),
+    new Employee("Pavel", 42),
+    new Employee("Anton", 17),
+    new Employee("Denis", 17),
+    new Employee("Artem", 23),
+};
+
+var oldGuys = from empl in myEmployees
+            where empl.Age > 30
+            select empl;
+
+var sortedGuys = from empl in myEmployees
+            orderby empl.Name
+            select empl;
+
+var uniqueGuys = from empl in myEmployees
+            group empl by empl.Age;
+
+var averageAge = myEmployees.Average(empl => empl.Age);
+
+Console.WriteLine("Only oldGuys > 35:");
+foreach (var item in oldGuys)
+{
+    Console.WriteLine(item.Name);
+}
+
+Console.WriteLine("Sorted by name:");
+foreach (var item in sortedGuys)
+{
+    Console.WriteLine(item.Name);
+}
+
+Console.WriteLine("Group by age:");
+foreach (var group in uniqueGuys)
+{
+    Console.WriteLine($"Group {group.Key} age:");
+    foreach (var empl in group)
+    {
+        Console.WriteLine(empl.Name);
+    }
+}
+
+Console.WriteLine($"Average age: {(int)averageAge}");
+
+
+public class Employee
+{
+    public string Name {get;set;}
+    public int Age {get;set;}
+
+    public Employee(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
---
+++ Theory/2.C#/7. TPL & async-await/Theory2_7/Program.cs
@@ -0,0 +1,29 @@
+﻿var loaddataTask = LoadData();
+var processdataTask = ProcessData();
+var savedataTask = SaveData();
+
+await Task.WhenAll(loaddataTask,processdataTask,savedataTask);
+
+Console.WriteLine("All done!");
+
+
+static async Task LoadData()
+{
+    Console.WriteLine("Start LoadData()");
+    await Task.Delay(3000);
+    Console.WriteLine("End LoadData()");
+}
+
+static async Task ProcessData()
+{
+    Console.WriteLine("Start ProcessData()");
+    await Task.Delay(2000);
+    Console.WriteLine("End ProcessData()");
+}
+
+static async Task SaveData()
+{
+    Console.WriteLine("Start SaveData()");
+    await Task.Delay(1000);
+    Console.WriteLine("End SaveData()");
+}
