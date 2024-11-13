eb12301ef74fe888f6879c8631470839952b929a
Roman <Newtrr@yandex.ru>
2024-11-13 22:17:23 +0300

    vvv

--- Practice/ModsenPractice/obj/Debug/net8.0/ModsenPractice.AssemblyInfo.cs
+++ Practice/ModsenPractice/obj/Debug/net8.0/ModsenPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModsenPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f9bb3b0925a7d4eea479eff335cc157704a901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15467629d9e08720223366a84465d1e16a78d69f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModsenPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModsenPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
